7300f2757b08666379db5fa36eeb374680a83987
duende84 <andreselduende@gmail.com>
2012-08-21 10:02:55 -0500

    Add user following -> Chapter 11

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,27 +4,30 @@ describe "Static pages" do
 
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
+  describe "for signed-in users" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      FactoryGirl.create(:micropost, user: user, content: "Lorem")
+      FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+      sign_in user
+      visit root_path
+    end
 
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
+    it "should render the user's feed" do
+      user.feed.each do |item|
+        page.should have_selector("li##{item.id}", text: item.content)
+      end
+    end
 
-    describe "for signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
+    describe "follower/following counts" do
+      let(:other_user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-        sign_in user
+        other_user.follow!(user)
         visit root_path
       end
 
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          page.should have_selector("li##{item.id}", text: item.content)
-        end
-      end
+      it { should have_link("0 following", href: following_user_path(user)) }
+      it { should have_link("1 followers", href: followers_user_path(user)) }
     end
   end
 
@@ -49,85 +52,3 @@ describe "Static pages" do
     it { should have_selector('title', text: full_title('Contact')) }
   end
 end
-
-
-
-
-=begin
-require 'spec_helper'
-
-describe "Static pages" do
-
-  describe "Home page" do
-
-    it "should have the h1 'Sample App'" do
-      visit root_path
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', :text => '| Home')
-    end
-  end
-
-  describe "Help page" do
-
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title" do
-      visit help_path
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit help_path
-      page.should_not have_selector('title', :text => '| Help')
-    end
-  end
-
-  describe "About page" do
-
-    it "should have the h1 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title" do
-      visit about_path
-      page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit about_path
-      page.should_not have_selector('title', :text => '| About Us')
-    end
-  end
-
-  describe "Contact page" do
-
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | Contact")
-    end
-  end
-end
-
-=end
